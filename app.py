4dcbec829df863bf312b6c69cde8fdffb77373d3
wanjikumwangib@gmail.com <wanjikumwangib@gmail.com>
2022-11-23 06:29:55 +0300

    more dependancies and clean up app file

--- app.py
+++ app.py
@@ -10,19 +10,10 @@ from resources.user import blp as UserBlueprint
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config["API_TITLE"] = "Users REST API"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = "postgresql+psycopg2://beth:Coded101@localhost/userdb"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["PROPAGATE_EXCEPTIONS"] = True
     register_extensions(app)
     register_resources(app)
 
--- config.py
+++ config.py
@@ -1,11 +1,12 @@
+import os
+
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://beth:Coded101@localhost/userdb"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     API_TITLE = "Users REST API"
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0.3"
     OPENAPI_URL_PREFIX = "/"
-    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
-    OPENAPI_SWAGGER_UI_URL= "https://cdnjsdelivrnet/npm/swagger-ui-dist/"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://beth:Coded101@localhost/userdb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
+    CACHE_DEFAULT_TIMEOUT = 10 * 60
